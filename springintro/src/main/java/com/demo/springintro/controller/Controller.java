02c00d601061f3f22e8a1f1f24c4b37b334bc6e5
Adarshpatel0308 <adarshpatel88899@gmail.com>
2025-02-21 20:47:46 +0530

    [Adarsh Patel] add. Spring App using MVC controller and Thymeleaf to display the Model

--- springintro/src/main/java/com/demo/springintro/controller/Controller.java
+++ springintro/src/main/java/com/demo/springintro/controller/Controller.java
@@ -1,16 +1,24 @@
 package com.demo.springintro.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller // Use @Controller instead of @RestController
 @RequestMapping("/hello")
-
 class HelloController {
 
     @GetMapping("/UC1")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/UC2")
+    public String sayHello(Model model) {
+        // Add data to the model
+        model.addAttribute("message", "Hello from BridgeLabz");
+        // Return the view name (hello.html)
+        return "hello";
     }
 }
